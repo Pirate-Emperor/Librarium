db33104f7808fa782bd3cd111d8d348908875052
Pirate-Emperor <piratekingrahul@gmail.com>
2022-12-22 11:51:00 +0100

    Add src\main\java\com\PirateEmperor\Librarium\httpHandlers\EmailInfo.java

---
+++ src/main/java/com/PirateEmperor/Librarium/httpHandlers/EmailInfo.java
@@ -0,0 +1,19 @@
+package com.PirateEmperor.Librarium.httpHandlers;
+
+public class EmailInfo {
+	private String email;
+	
+	public EmailInfo() {}
+	
+	public EmailInfo(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
